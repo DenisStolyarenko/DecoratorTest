8f8a13b7c9de188c2d775a3840ca2e8eccbced6e
Denis Stolyarenko <denis_stolyarenko@epam.com>
2017-09-15 14:26:32 +0600

    Update 9/15/2017 2 version

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,20 +1,13 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends AbstractPage {
-//    private final By USER_NAME_INPUT_LOCATOR = By.xpath("//input[@name='username']");
-//    private final By PASSWORD_INPUT_LOCATOR = By.xpath("//input[@name='password']");
-//    private final By LOGIN_BUTTON_LOCATOR = By.xpath("//input[@name='Login']");
-//    private final By LOGIN_PAGE_TEXT_LOCATOR = By.xpath("//td[@class='header1']/h1");
 
     @FindBy(xpath = "//input[@name='username']")
-    private WebElement userNameInput;
-    private Decorator userNameInputD;
+    private Decorator userNameInput;
 
     @FindBy(xpath = "//input[@name='password']")
-    private WebElement passwordInput;
-    private Decorator passwordInputD;
+    private Decorator passwordInput;
 
     @FindBy(xpath = "//input[@name='Login']")
     private Decorator loginButton;
